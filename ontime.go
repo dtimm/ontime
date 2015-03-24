3f0b0a18cdf5a33f5ca43553044923ab540eb6df
David Timm <timmmeister@gmail.com>
2015-03-24 14:00:47 -0600

    Changed function structure to make more sense, started groundwork for input and output formatting from the command line.

--- ontime.go
+++ ontime.go
@@ -5,64 +5,88 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
 	"time"
 )
 
+var now time.Time
+var outFmt string
+var inFmt string
+var inTime string
+
 func main() {
-	outTime := flag.String("ot", "std", "Output time clock. standard, binary, or metric.")
-	//outForm := flag.String("of", " ", "Output time format.")
-	//inForm := flag.String("if", " ", "Input time format.")
+	outTime := flag.String("ot", "std", "Output time clock. standard, binary,"+
+		"or metric.")
+	outForm := flag.String("of", " ", "Output time format.")
+	inForm := flag.String("if", " ", "Input time format.")
 	flag.Parse()
 
+	inTime = flag.Arg(0)
+	outFmt = *outForm
+	inFmt = *inForm
+
+	now = time.Now()
 	switch *outTime {
-		case "std", "standard", "s":
-			fmt.Printf("%d", todaySecStd() / 1000000000)
-		case "met", "metric", "m":
-			PrintMetTime()
-		case "bin", "b", "binary", "hex":
-			PrintHexTime()
+	case "std", "standard", "s":
+		fmt.Printf("%02d:%02d", now.Hour(), now.Minute())
+	case "met", "metric", "m":
+		PrintMetTime()
+	case "bin", "b", "binary", "hex":
+		PrintHexTime()
 	}
 }
 
-func todaySecStd () int64 {
-	now := time.Now()
-	return int64((now.Second() + 60 * now.Minute() + 3600 * now.Hour()) * 1000000000 + now.Nanosecond())
+func stdToday() int64 {
+	return int64((now.Second()+60*now.Minute()+3600*now.Hour())*1000000000 + now.Nanosecond())
 }
 
-func todaySecMet () int64 {
-	return todaySecStd() * 1000 / 864
+func metToday() int64 {
+	return stdToday() * 1000 / 864
 }
 
-func todaySecBin () int64 {
-	return todaySecStd() * 65536 / 86400
+func binToday() int64 {
+	return stdToday() * 65536 / 86400
 }
 
-func BinToStd(binSecs int64) int64 {
-	return binSecs * 86400 / 65536
+func binHour() int {
+	return int(binToday()/1000000000) / 4096
 }
 
-func MetToStd(metSecs int64) int64 {
-	return metSecs * 864 / 1000
+func binMinute() int {
+	return int(binToday()/1000000000) % 4096 / 256
 }
 
-func BinToMet(binSecs int64) int64 {
-	return binSecs * 100000 / 65536
+func binSecond() int {
+	return int(binToday()/1000000000) % 256
 }
 
-func MetToBin(metSecs int64) int64 {
-	return metSecs * 65536 / 100000
+func binNanosecond() int64 {
+	return binToday() % 65536
 }
 
+func metHour() int {
+	return int(metToday() / 10000000000000)
+}
+
+func metMinute() int {
+	return int(metToday()/1000000000) % 10000 / 100
+}
+
+func metSecond() int {
+	return int(metToday()/1000000000) % 100
+}
+
+func metNanosecond() int64 {
+	return metToday() / 1000000000000000
+}
+
+// Print binary time HM
 func PrintHexTime() {
-	fmt.Printf("%X", todaySecBin() / 1000000000)
+	fmt.Printf("%X%X", binHour(), binMinute())
 }
 
+// Print metric time HH:MM
 func PrintMetTime() {
-	now := todaySecMet() / 1000000000
-	hour := now / 10000
-	minute := now % 10000 / 100
-	second := now % 100
-	fmt.Printf("%2d:%2d:%2d", hour, minute, second)
+	fmt.Printf("%02d:%02d", metHour(), metMinute())
 }
--- ontime_test.go
+++ ontime_test.go
@@ -3,25 +3,11 @@
 package main
 
 import (
-	"testing"
 	"fmt"
-	)
+	"testing"
+)
 
-func Test01mb(t *testing.T) {
-	if MetToBin(0) != 0 {
-		t.Fail()
-	}
-	fmt.Println("0met = 0bin")
+func TestMain(m *testing.M) {
+	fmt.Printf("The current time is: %s", time.Now())
+	os.Exit(m.Run())
 }
-
-func Test02mb(t *testing.T) {
-	if MetToBin(50000) != 32768 {
-		t.Fail()
-	}
-	fmt.Println("50000met = 32768bin")
-}
-
-//func TestMain(m *testing.M) {
-//	fmt.Printf("The current time is: %s", time.Now())
-//	os.Exit(m.Run())
-//}
