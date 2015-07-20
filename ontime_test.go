eea6d996d484a42dee1812b1fc841c055cd41106
David Timm <timmmeister@gmail.com>
2015-07-20 11:34:43 -0600

    Made testing work at all.

--- ontime_test.go
+++ ontime_test.go
@@ -1,13 +1,24 @@
 // Test cases for OnTime.
 
-package main
+package ontime
 
 import (
-	"fmt"
+	"os"
+	"time"
 	"testing"
 )
 
+func TestBinTime(t *testing.T) {
+	now, _ = time.Parse("3:04pm", "12:34pm")
+	if binHour() != 8 {
+		t.Error("For binHour, expected 8, got", binHour())
+	}
+	if binMinute() != 6 {
+		t.Error("For binMinute, expected 6, got", binMinute())
+	}
+}
+
 func TestMain(m *testing.M) {
-	fmt.Printf("The current time is: %s", time.Now())
+	//fmt.Printf("The current time is: %s", time.Now())
 	os.Exit(m.Run())
 }
